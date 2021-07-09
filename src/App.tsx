62961ceed963069cb1d4b4811b1665d707af7255
Kristoffer Karlsson <karlsson.kristoffer93@gmail.com>
2021-07-09 20:36:11 +0200

    Adding firebase and confs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as Firebase from './services/Firebase'
 
 function App() {
+  Firebase.initFirebase();
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/services/Firebase.ts
@@ -0,0 +1,19 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURE_ID
+};
+
+export const initFirebase = () => firebase.initializeApp(firebaseConfig);
