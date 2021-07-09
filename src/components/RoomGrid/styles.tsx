0ea502e4864d3a27386cdda4ddfe98118ac09a62
Benny Carlsson <benny.carlsson@humblebee.se>
2021-07-09 23:04:00 +0200

    feat: Grid

--- src/components/RoomGrid/styles.tsx
+++ src/components/RoomGrid/styles.tsx
@@ -1,13 +1,24 @@
 import styled from 'styled-components';
 
-export const GridContainer = styled.div`
+type GridContainerType = {
+  rows: number;
+  columns: number;
+};
+
+export const GridContainer = styled.div<GridContainerType>`
   display: grid;
-  grid-template-rows: repeat(5, 25px);
-  grid-template-columns: repeat(5, 25px);
-  column-gap: 5px;
-  row-gap: 5px;
+  grid-template-rows: ${({ rows }) => `repeat(${rows}, 25px)`};
+  grid-template-columns: ${({ columns }) => `repeat(${columns}, 25px)`};
+  column-gap: 1px;
+  row-gap: 1px;
+  justify-content: center;
 `;
 
 export const GridItem = styled.div`
   border: 1px solid black;
+  font-size: 8px;
+  color: gray;
+  &.Mark {
+    background-color: gray;
+  }
 `;
